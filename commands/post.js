8cf5cedfb1441e2a53cd8158f7e36c2a19091b2b
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-02 21:49:21 +0200

    Update post.js

--- commands/post.js
+++ commands/post.js
@@ -7,15 +7,25 @@ module.exports.run = async(client, message, args) => {
         .setColor("#14e378");
         message.channel.send(embed);
 
+        const filter = m => m.content.includes('discord.js');
+        var kanaal = message.guild.channels.cache.get('739234706142527559');
+
     
      message.channel.awaitMessages(m => m.author.id == message.author.id,
                 { max: 1, time: 30000 }).then(collected => {
  
                     if (collected.first().content.toLowerCase() == 'suggestie') {
 
+                        const sugCollector = message.channel.createMessageCollector(filter, {time: 15000});
 
-                        var kanaal = message.guild.channels.cache.get('739234706142527559') 
-                        kanaal.send("Test");
+                        var embedSug = new discord.MessageEmbed()
+                        .setTitle("Nieuwe suggestie!")
+                        .addField("Suggestie van:", message.author)
+                        .addField("Suggestie:", sugCollector)
+
+                       collector.on('collect', m => {
+                           kanaal.send(embedSug)
+                       })
  
                        
                     } else if (collected.first().content.toLowerCase() == 'bug') {
