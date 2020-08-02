32e0721258ea47ebd4186393a221b8dd6cef7762
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-02 22:06:54 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,25 +2,14 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    var geenPermsEmbed = new discord.MessageEmbed()
-        .setDescription(`⛔ ${message.author} jij hebt hier geen permissions voor.`)
-        .setColor("#31d697");
-
-    var args1Embed = new discord.MessageEmbed()
-        .setDescription(`⛔ ${message.author} Geef een gebruiker op.`)
-        .setColor("#31d697");
-
-    var args2Embed = new discord.MessageEmbed()
-        .setDescription(`⛔ ${message.author} Geef een reden op.`)
-        .setColor("#31d697");
-
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(geenPermsEmbed);
+    
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Geen perms");
 
     var gebruiker = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
     var reden = args.slice(2).join(" ");
 
-    if (!gebruiker) return message.channel.send(args1Embed);
-    if (!reden) return message.channel.send(args2Embed);
+    if (!gebruiker) return message.channel.send("Geen Geruiker");
+    if (!reden) return message.channel.send("Geen reden");
 
     var banEmbed = new discord.MessageEmbed()
         .setDescription(`✅ ${message.author} ik heb ${gebruiker} succesvol geband.`)
