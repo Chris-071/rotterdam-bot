c41828c85078b1b3e81b728e893badf2db937fed
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-03 18:46:26 +0200

    Update gang.js

--- commands/gang.js
+++ commands/gang.js
@@ -3,8 +3,33 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
 
     var embed = new discord.MessageEmbed()
-    .setDescription("Van welke gang wil je lid worden?")
-    .addField("The Crimenals", "Info: \n Owner: gevuldenkoek")
+    .setDescription("Wil je lid worden van:")
+    .addField("The Criminals", "Info: \n Owner: gevuldenkoek")
+    .setColor("#14e378");
+
+    var role = message.guild.roles.cache.get('739885903173910568');
+
+    message.channel.awaitMessages(m => m.author.id == message.author.id,
+        { max: 1, time: 30000 }).then(collected => {
+
+            if (collected.first().content.toLowerCase() == 'ja') {
+                message.member.roles.add(role);
+
+                var jaEmbed = new discord.MessageEmbed()
+                .setDescription("Je bent lid van de gang!")
+                .setColor("#14e378");
+                message.channel.send(jaEmbed);
+            } else if (collected.first().content.toLowerCase() == 'nee') {
+                var neeEmbed = new discord.MessageEmbed()
+                .setDescription("Je bent geen lid van de gang.")
+                .setColor("#14e378");
+                message.channel.send(neeEmbed)
+            }
+            
+
+        }).catch(err => {
+            message.reply("Error")
+        });
 
 
 }
