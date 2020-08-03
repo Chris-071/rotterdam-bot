1e6ce4d2763f93ee2108ef49ef509efabe502818
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-03 17:27:05 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -4,14 +4,16 @@ module.exports.run = async(client, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Geen perms");
 
-    var poll = args.slice(1).join(" ");
+    var poll = args.slice(0).join(" ");
 
     var embed = new discord.MessageEmbed()
     .setDescription(`📊Poll - ${poll}`)
-    .setColor("#14e378")
+    .setColor("#14e378");
 
      var kanaal = message.guild.channels.cache.get("739234706142527559");
     kanaal.send(embed);
+
+    message.channel.send("Poll gemaakt!" + poll);
 
 }
 
