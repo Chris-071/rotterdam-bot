d9e55915675d8aa7c0b0e469d805ad5a13b48317
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-05 11:59:48 +0200

    Update gang.js

--- commands/gang.js
+++ commands/gang.js
@@ -2,40 +2,40 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-    // var embed = new discord.MessageEmbed()
-    // .setDescription("Wil je lid worden van: <ja/nee>")
-    // .addField("The Criminals", "Owner: gevuldenkoek")
-    // .setColor("#14e378");
-    // message.channel.send(embed);
+     var embed = new discord.MessageEmbed()
+    .setDescription("Wil je lid worden van: <ja/nee>")
+     .addField("The Criminals", "Owner: gevuldenkoek")
+     .setColor("#14e378");
+     message.channel.send(embed);
 
-    // var role = message.guild.roles.cache.get('739885903173910568');
+    var role = message.guild.roles.cache.get('739885903173910568');
 
-    // message.channel.awaitMessages(m => m.author.id == message.author.id,
-    //     { max: 1, time: 30000 }).then(collected => {
+     message.channel.awaitMessages(m => m.author.id == message.author.id,
+         { max: 1, time: 30000 }).then(collected => {
 
-    //         if (collected.first().content.toLowerCase() == 'ja') {
-    //             message.member.roles.add(role);
+             if (collected.first().content.toLowerCase() == 'ja') {
+                 message.member.roles.add(role);
 
-    //             var jaEmbed = new discord.MessageEmbed()
-    //             .setDescription("Je bent lid van de gang!")
-    //             .setColor("#14e378");
-    //             message.channel.send(jaEmbed);
-    //         } else if (collected.first().content.toLowerCase() == 'nee') {
-    //             var neeEmbed = new discord.MessageEmbed()
-    //             .setDescription("Je bent geen lid van de gang.")
-    //             .setColor("#14e378");
-    //             message.channel.send(neeEmbed)
-    //         }
+                 var jaEmbed = new discord.MessageEmbed()
+                 .setDescription("Je bent lid van de gang!")
+                .setColor("#14e378");
+                 message.channel.send(jaEmbed);
+             } else if (collected.first().content.toLowerCase() == 'nee') {
+                 var neeEmbed = new discord.MessageEmbed()
+                 .setDescription("Je bent geen lid van de gang.")
+               .setColor("#14e378");
+             message.channel.send(neeEmbed)
+         }
             
 
-    //     }).catch(err => {
-    //         message.reply("Error")
-    //     });
+         }).catch(err => {
+             message.reply("Error")
+           });
 
-    var embed = new discord.MessageEmbed()
-    .setDescription("Lid worden van deze gang is niet meer mogelijk.")
-    .setColor("#14e378");
-    message.channel.send(embed);
+  //  var embed = new discord.MessageEmbed()
+ //   .setDescription("Lid worden van deze gang is niet meer mogelijk.")
+    //.setColor("#14e378");
+ //   message.channel.send(embed);
 
 
 }
