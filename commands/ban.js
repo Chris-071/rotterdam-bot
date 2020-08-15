db42a15aaa38d085b6ce3d9853e3fb8f78d44ae6
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-15 23:52:36 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 
-module.exports.run = async(client, message, args) =>l
+module.exports.run = async(client, message, args) => {
 
 var embed = new discord.MessageEmbed()
        .setDescription("<:fout:730747907480748082> Deze command is op dit moment in onderhoud, Probeer het later opnieuw.")
