515e472f56cd2cc1b7a609dc078293c94ccad040
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-15 23:49:13 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,9 +2,12 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) =>l
 
-Var embed = new discord.MessageEmbed()
+var embed = new discord.MessageEmbed()
        .setDescription("<:fout:730747907480748082> Deze command is op dit moment in onderhoud, Probeer het later opnieuw.")
         .setColor("#14e378");
+
+
+message.channel.send(embed)
 
 }
 
