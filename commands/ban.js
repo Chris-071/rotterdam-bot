8e6ea60c4c64cb5ad6a520cc564070bc457a86f6
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-15 23:45:54 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,40 +1,9 @@
 const discord = require("discord.js");
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = async(client, message, args) =>l
 
-    if(!message.member.hasPermission("BAN_MEMBERS")) return("Alleen staff-leden kunnen dit.");
-
-    if(!args[0]) return message.reply("Geef een Gebruiker op.");
-    if(!args[1]) return message.reply("Geef een Reden op.");
-
-    var user = message.mentions.users.first() || message.guild.members.get(args[0]);    
-    if(!user) return message.reply("Geef een Gebruiker op.");
-
-    var reden = args.slice(2).join(" ");
-    if(!reden) return message.reply("Geef een Reden op.");
-
-    var mainEmbed = new discord.MessageEmbed()
-    .setTitle("Kick")
-    .setColor("#14e378")
-    .addField("Moderator:", message.author)
-    .addField("Gebruiker:", user)
-    .addField("Reden:", reden)
-    .addField("Tijd:", message.createdAt)
-
-    var kanaal = message.guild.channels.cache.get("733252337556127744");
-    kanaal.send(mainEmbed);
-
-    var embed2 = new discord.MessageEmbed()
-    .setColor("#14e378")
-    .setDescription(`<:Succesvol:730747906331770900> ${user} succesvol geband voor **${reden}**`);
-
-    message.channel.send(embed2);
-
-    user.ban(reden).catch(err => {
-        if(err) console.log(err);
-    });
-   
-    
+Var embed = new discord.MessageEmbed()
+.setFescription  
 
 }
 
