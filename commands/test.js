3f7e04f9f1907ba80d78401638773f227105d779
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-08-16 13:15:37 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
 
 var embed = new discord.MessageEmbed()
-       .setDescription(“<:Succesvol:730747906331770900> Test Geslaagd!”)
+       .setDescription("<:Succesvol:730747906331770900> Test Geslaagd!")
         .setColor("#14e378");
 
 
