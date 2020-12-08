62f85a5105703afe82427087105ec74c36b30bdf
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-12-08 19:46:40 +0100

    Update training.js

--- commands/training.js
+++ commands/training.js
@@ -8,23 +8,23 @@ module.exports.run = async (client, message, args) => {
         .setDescription("Wat is de `Eenheid` van de training?")
         .setColor("BLUE");
 
-        var type = new discord.MessageEmbed()
+    var type = new discord.MessageEmbed()
         .setDescription("Wat is de `Type` van de training?")
         .setColor("BLUE");
 
-        var tijd = new discord.MessageEmbed()
+    var tijd = new discord.MessageEmbed()
         .setDescription("Wat is de `Tijd` van de training")
         .setColor("BLUE");
 
-        var cohost = new discord.MessageEmbed()
+    var cohost = new discord.MessageEmbed()
         .setDescription("Wie is de `Co-Host` van de training")
         .setColor("BLUE");
 
-        var kijkersBewakers = new discord.MessageEmbed()
+    var kijkersBewakers = new discord.MessageEmbed()
         .setDescription("Zijn Kijkers // Bewakers toegestaan?")
         .setColor("BLUE");
 
-        var opmerkingen = new discord.MessageEmbed()
+    var opmerkingen = new discord.MessageEmbed()
         .setDescription("Heb je nog `Opmerkingen` voor de training?")
         .setColor("BLUE");
 
@@ -51,43 +51,51 @@ module.exports.run = async (client, message, args) => {
 
                     message.channel.awaitMessages(s => s.author.id == message.author.id, { max: 1 }).then(antwoord => {
                         var antwoord5 = antwoord.first();
-                        
-                       
-                        var uitkomst = new discord.MessageEmbed()
-                        .setTitle("Training is aangemaakt!")
-                        .setColor("BLUE")
-                        .setDescription(`Eenheid: ${antwoord1}. \Type: ${antwoord2}. \Tijd: ${antwoord3}. \Co-Host: ${antwoord4}. \Host: ${message.author}. \Host: ${antwoord5}.\Host: ${message.author}. \nIs dit oke? Reageer hieronder.`);
-    
-                        
-                    var gemaakt = new discord.MessageEmbed()
-                    .setTitle("Training is aangemaakt!")
-                    .setColor("BLUE");
-    
-                    var eventEmbed = new discord.MessageEmbed()
-                        .setTitle(`${antwoord1} Training.`)
-                        .addField("Eenheid:", antwoord1)
-                        .addField("Type Training:", antwoord2)
-                        .addField("Host:", message.author)
-                        .addField("Co-Host:", antwoord3)
-                        .setColor("BLUE");
-    
-                    message.channel.send(uitkomst).then(async msg => {
-    
-                        var emoji = await promptMessage(msg, message.author, 30, ["✔", "❌"]);
-    
-                        if (emoji === "✔") {
-    
-                            channel.send(eventEmbed);
-                            msg.edit(gemaakt);
-    
-                        } else if (emoji === "❌") {
-    
-                            message.channel.send("Event gecanceld");
-    
-                        }
-    
-                    })
+                        message.channel.send(opmerkingen);
 
+
+                        message.channel.awaitMessages(s => s.author.id == message.author.id, { max: 1 }).then(antwoord => {
+                            var antwoord6 = antwoord.first();
+
+
+                            var uitkomst = new discord.MessageEmbed()
+                                .setTitle("Training is aangemaakt!")
+                                .setColor("BLUE")
+                                .setDescription(`Eenheid: ${antwoord1}. \n Type: ${antwoord2}. \nTijd: ${antwoord3}. \nCo-Host: ${antwoord4}. \nHost: ${message.author}. \nBewakers/Kijkers toegestaan: ${antwoord5}. \nOpmerkingen: ${antwoord6}. \nIs dit oke? Reageer hieronder.`);
+
+
+                            var gemaakt = new discord.MessageEmbed()
+                                .setTitle("Training is aangemaakt!")
+                                .setColor("BLUE");
+
+                            var trainingEmbed = new discord.MessageEmbed()
+                                .setTitle(`${antwoord1} Training.`)
+                                .addField("Eenheid:", antwoord1)
+                                .addField("Type Training:", antwoord2)
+                                .addField("Host:", message.author)
+                                .addField("Co-Host:", antwoord3)
+                                .addField("Kijkers/Bewakers Toegestaan: ", antwoord5)
+                                .addField("Opmerkingen: ", antwoord6)
+                                .setColor("BLUE");
+
+                            message.channel.send(uitkomst).then(async msg => {
+
+                                var emoji = await promptMessage(msg, message.author, 30, ["✔", "❌"]);
+
+                                if (emoji === "✔") {
+
+                                    channel.send(trainingEmbed);
+                                    msg.edit(gemaakt);
+
+                                } else if (emoji === "❌") {
+
+                                    message.channel.send("Event gecanceld");
+
+                                }
+
+                            })
+
+                        })
                     })
                 })
             })
