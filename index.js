0de24b33eacc89c677127e333f372042dcb01b46
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-12-16 17:37:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new discord.Client();
 client.commands = new discord.Collection();
 client.login(process.env.token)
 const fs = require("fs");
+const { message } = require("noblox.js");
 
 const activiteiten = [
     `!help`,
@@ -84,3 +85,70 @@ client.on("message", async message => {
 
 
 });
+
+
+if (command === `${prefix}shutdown`) {
+    if (message.author.id === "421717020288221186") {
+
+        const wachtTijdTussen = "1000"
+        const wachtijdSd = "2000"
+
+        var embed1 = new discord.MessageEmbed()
+            .setDescription(`Proces opstarten..`)
+            .setColor("BLUE");
+
+        var embed2 = new discord.MessageEmbed()
+            .setDescription(`Proces laden..`)
+            .setColor("BLUE");
+
+        var embed3 = new discord.MessageEmbed()
+            .setDescription(`Proces starten..`)
+            .setColor("BLUE");
+
+        var embed4 = new discord.MessageEmbed()
+            .setDescription(`Proces klaarmaken..`)
+            .setColor("BLUE");
+
+        var embed5 = new discord.MessageEmbed()
+            .setDescription(`Proces opgestart!`)
+            .setColor("BLUE");
+
+        var embed6 = new discord.MessageEmbed()
+            .setDescription(`Shutdown klaarmaken..`)
+            .setColor("BLUE");
+
+        var embed7 = new discord.MessageEmbed()
+            .setDescription(`Shuting Down..`)
+            .setFooter(`Systeem is uitgeschakeld. Wacht tot de bot is uitgeschakeld en start hem dan opnieuw op.`)
+            .setColor("BLUE");
+
+        message.channel.send(embed1)
+
+        setTimeout(() => {
+            message.channel.send(embed2);
+        }, wachtTijdTussen)
+        .then(setTimeout(() => {
+            message.channel.send(embed3);
+        }, wachtTijdTussen))
+        .then(setTimeout(() => {
+            message.channel.send(embed4);
+        }, wachtTijdTussen))
+
+        .then(setTimeout(() => {
+            message.channel.send(embed5);
+        }, wachtTijdTussen))
+
+        .then(setTimeout(() => {
+            message.channel.send(embed6);
+        }, wachtTijdTussen))
+
+        .then(setTimeout(() => {
+            message.channel.send(embed7);
+            client.destroy(token);
+        }, wachtijdSd))
+
+
+    } else {
+        message.reply(`Alleen de bot maken kan dit.`)
+    }
+}
