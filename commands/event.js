77cd135aab8f4486d94facc39a85cb501f82e8c0
Chris-071 <64900824+Chris-071@users.noreply.github.com>
2020-12-18 17:50:30 +0100

    Update event.js

--- commands/event.js
+++ commands/event.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
+    if (!message.member.roles.cache.get('785596843391582269')) return;
 
     var tijd = new discord.MessageEmbed()
         .setDescription("Wat is de `Tijd` van het event?")
